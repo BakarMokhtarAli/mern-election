eabf527ccd73e96e029b6ea7beda8556b7a0b595
abuukar <walaalpakar08@gmail.com>
2024-07-17 18:18:02 +0300

    correct forntend links

--- frontend/src/store/api/VoterSlice.js
+++ frontend/src/store/api/VoterSlice.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const BASE_URL = "http://localhost:8000/api/v1";
+const BASE_URL = "https://mern-election.onrender.com/api/v1";
 
 const voter = createApi({
   reducerPath: "voter",
