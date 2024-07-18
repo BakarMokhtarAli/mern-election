cffb3464afa986a7817db5159a53c56e1c4e8cbe
abuukar <walaalpakar08@gmail.com>
2024-07-18 11:36:25 +0300

    closed candidate navigation by voter

--- frontend/src/pages/candidate/CandidateHome.jsx
+++ frontend/src/pages/candidate/CandidateHome.jsx
@@ -1,8 +1,21 @@
+import { useNavigate } from "react-router-dom";
 import { SideBar, Footer, Hero } from "../../components/candidate";
 import { useTitle } from "../../Hooks/useTitle";
+import { useEffect } from "react";
 
 export const CandidateHome = () => {
   useTitle("Candidate");
+  const navigate = useNavigate();
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
+  useEffect(() => {
+    if (currentUser?.role == "voter") {
+      navigate("/dashboard");
+      console.log(true);
+    } else {
+      console.log(false);
+    }
+  }, []);
   return (
     <div>
       <SideBar />
